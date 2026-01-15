d0fd56cb143f6a15c3b3ee044e607bf9c6a71dcd
JOEL FERREIRA DA SILVA <joelferreiradasilva16@gmail.com>
2026-01-15 00:12:27 -0300

    Modify nmap command to use network IP placeholder
    Updated command to include a placeholder for network IP.

--- vulns.py
+++ vulns.py
@@ -4,7 +4,7 @@ print("--- Iniciando Busca de Vulnerabilidades (NSE) ---")
 print("Isso pode demorar alguns minutos...")
 
 # O script 'vuln' do nmap verifica as falhas mais conhecidas
-comando = "nmap -sV --script vuln 192.168.15.1 > storage/downloads/vulnerabilidades.txt"
+comando = "nmap -sV --script vuln "ip da rede a se verificar"> storage/downloads/vulnerabilidades.txt"
 
 os.system(comando)
 
